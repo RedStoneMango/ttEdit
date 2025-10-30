18330006413fee6dfd740e4ead9afaa6b74008a3
RedstoneMango <fabian@familie-krohn.de>
2025-10-30 16:11:38 +0100

    implement project folder listing

--- src/main/java/io/github/redstonemango/ttedit/front/controller/ProjectListController.java
+++ src/main/java/io/github/redstonemango/ttedit/front/controller/ProjectListController.java
@@ -1,5 +1,80 @@
 package io.github.redstonemango.ttedit.front.controller;
 
+import io.github.redstonemango.mangoutils.OperatingSystem;
+import io.github.redstonemango.ttedit.Launcher;
+import io.github.redstonemango.ttedit.front.UXUtilities;
+import io.github.redstonemango.ttedit.front.projectEntry.ProjectEntry;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Objects;
+
 public class ProjectListController {
 
+    @FXML private ListView<File> projectView;
+    @FXML private TextField filterField;
+
+    ObservableList<File> projects = FXCollections.observableArrayList();
+    FilteredList<File> filteredProjects = new FilteredList<>(projects, File::isDirectory);
+
+    @FXML
+    private void initialize() {
+        filterField.textProperty().addListener((_, _, filter) ->
+            filteredProjects.setPredicate(file -> file.isDirectory()
+                    && file.getName().toLowerCase().contains(filter.toLowerCase()))
+        );
+        projectView.setItems(filteredProjects);
+
+        projectView.sceneProperty().addListener((_, _, sc) -> {
+            if (sc != null) {
+                sc.addEventFilter(KeyEvent.KEY_PRESSED, e -> {
+                    boolean controlDown = OperatingSystem.isMac() ? e.isMetaDown() : e.isControlDown();
+                    if (e.getCode() == KeyCode.R && controlDown) {
+                        updateProjects();
+                    }
+                });
+            }
+        });
+
+        UXUtilities.applyCustomCellFactory(projectView, file ->
+                ProjectEntry.build(file, () -> deleteProject(file), projectView)
+        );
+
+        updateProjects();
+    }
+
+    @FXML
+    private void onOpenFolder() {
+        OperatingSystem.loadCurrentOS().open(Launcher.PROJECTS_HOME);
+    }
+
+    @FXML
+    private void onAddProject() {
+
+    }
+
+    private void deleteProject(File file) {
+
+    }
+
+    private void updateProjects() {
+        projects.setAll(
+                Arrays.stream(
+                        Objects.requireNonNullElse(
+                                Launcher.PROJECTS_HOME.list(),
+                                new String[0])
+                        )
+                    .map(s -> new File(Launcher.PROJECTS_HOME, s))
+                    .filter(File::isDirectory)
+                    .toList()
+        );
+    }
 }
---
+++ src/main/java/io/github/redstonemango/ttedit/front/projectEntry/ProjectEntry.java
@@ -0,0 +1,45 @@
+package io.github.redstonemango.ttedit.front.projectEntry;
+
+import io.github.redstonemango.ttedit.front.UXUtilities;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ProjectEntry {
+
+    @FXML private Label nameLabel;
+    @FXML private Circle deleteBgCircle;
+
+    void init(File file, Runnable onDelete) {
+        nameLabel.setText(file.getName());
+
+        UXUtilities.registerHoverAnimation(deleteBgCircle, true);
+        deleteBgCircle.setOnMousePressed(_ -> onDelete.run());
+    }
+
+    public static HBox build(File file, Runnable onDelete, ListView<?> view) {
+        FXMLLoader loader = new FXMLLoader(ProjectEntry.class.getResource(
+                "/io/github/redstonemango/ttedit/fxml/project-entry.fxml"));
+
+        HBox element;
+        try {
+            element = loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        ProjectEntry controller = loader.getController();
+        controller.init(file, onDelete);
+
+        element.prefWidthProperty().bind(view.widthProperty().subtract(20));
+
+        return element;
+    }
+
+}
