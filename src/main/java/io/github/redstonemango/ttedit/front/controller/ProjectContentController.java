4b4aa5d28f30c17eb910898ee20ac7604fb2c046
RedstoneMango <RedStoneMango@outlook.com>
2025-11-02 13:23:08 +0100

    add project-content UI

---
+++ src/main/java/io/github/redstonemango/ttedit/front/controller/ProjectContentController.java
@@ -0,0 +1,21 @@
+package io.github.redstonemango.ttedit.front.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Rectangle;
+
+public class ProjectContentController {
+
+    @FXML private Rectangle iconBackground;
+    @FXML private Label iconText;
+    @FXML private Label nameLabel;
+    @FXML private HBox addScriptControl;
+    @FXML private HBox addPageControl;
+    @FXML private HBox editItemControl;
+    @FXML private HBox cloneItemControl;
+    @FXML private HBox deleteItemControl;
+    @FXML private HBox configureProjectControl;
+    @FXML private HBox saveProjectControl;
+
+}
