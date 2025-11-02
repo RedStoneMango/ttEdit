bd0b5397e57f6b99ee6c2ceb3c9b475e4cfc8a7c
RedstoneMango <fabian@familie-krohn.de>
2025-11-02 14:30:27 +0100

    implement project-content-view opening logic

--- src/main/java/io/github/redstonemango/ttedit/front/controller/ProjectContentController.java
+++ src/main/java/io/github/redstonemango/ttedit/front/controller/ProjectContentController.java
@@ -1,5 +1,7 @@
 package io.github.redstonemango.ttedit.front.controller;
 
+import io.github.redstonemango.ttedit.back.Project;
+import io.github.redstonemango.ttedit.front.UXUtilities;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
@@ -10,6 +12,7 @@ public class ProjectContentController {
     @FXML private Rectangle iconBackground;
     @FXML private Label iconText;
     @FXML private Label nameLabel;
+    @FXML private HBox projectTitle;
     @FXML private HBox addScriptControl;
     @FXML private HBox addPageControl;
     @FXML private HBox editItemControl;
@@ -17,5 +20,22 @@ public class ProjectContentController {
     @FXML private HBox deleteItemControl;
     @FXML private HBox configureProjectControl;
     @FXML private HBox saveProjectControl;
+
+    @FXML
+    private void initialize() {
+        UXUtilities.registerHoverAnimation(projectTitle);
+        UXUtilities.registerHoverAnimation(addScriptControl);
+        UXUtilities.registerHoverAnimation(addPageControl);
+        UXUtilities.registerHoverAnimation(editItemControl);
+        UXUtilities.registerHoverAnimation(cloneItemControl);
+        UXUtilities.registerHoverAnimation(deleteItemControl);
+        UXUtilities.registerHoverAnimation(configureProjectControl);
+        UXUtilities.registerHoverAnimation(saveProjectControl);
+
+        Project project = Project.getCurrentProject();
+        nameLabel.setText(project.name());
+        iconText.setText(UXUtilities.determineAbbreviation(project.name()));
+        iconBackground.setFill(UXUtilities.determineColor(project.name()));
+    }
 
 }
