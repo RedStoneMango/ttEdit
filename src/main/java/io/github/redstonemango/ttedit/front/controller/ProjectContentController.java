4e2895e4d0662ec83bf3b44465ac4015760ef286
RedstoneMango <RedStoneMango@outlook.com>
2025-11-05 16:54:39 +0100

    implement project closing logic

--- src/main/java/io/github/redstonemango/ttedit/front/controller/ProjectContentController.java
+++ src/main/java/io/github/redstonemango/ttedit/front/controller/ProjectContentController.java
@@ -1,11 +1,23 @@
 package io.github.redstonemango.ttedit.front.controller;
 
+import io.github.redstonemango.ttedit.TtEdit;
 import io.github.redstonemango.ttedit.back.Project;
 import io.github.redstonemango.ttedit.front.UXUtilities;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Point2D;
+import javafx.scene.Scene;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ProjectContentController {
 
@@ -20,6 +32,8 @@ public class ProjectContentController {
     @FXML private HBox deleteItemControl;
     @FXML private HBox configureProjectControl;
     @FXML private HBox saveProjectControl;
+
+    private ContextMenu cxtMenu;
 
     @FXML
     private void initialize() {
@@ -36,6 +50,65 @@ public class ProjectContentController {
         nameLabel.setText(project.name());
         iconText.setText(UXUtilities.determineAbbreviation(project.name()));
         iconBackground.setFill(UXUtilities.determineColor(project.name()));
+
+        MenuItem configureItem = new MenuItem("Configure Project");
+        configureItem.setOnAction(_ -> onConfigure());
+        MenuItem saveItem = new MenuItem("Save Whole Project");
+        saveItem.setOnAction(_ -> onSave());
+        MenuItem closeItem = new MenuItem("Close Project");
+        closeItem.setOnAction(_ -> {
+            Platform.runLater(this::close);
+        });
+
+        cxtMenu = new ContextMenu(
+                configureItem, saveItem,
+                new SeparatorMenuItem(),
+                closeItem
+        );
     }
 
+    @FXML
+    private void onProjectTitle() {
+        if (cxtMenu.isShowing()) {
+            cxtMenu.hide();
+        }
+        else {
+            Point2D titlePos = projectTitle.localToScreen(0, 0);
+            cxtMenu.show(
+                    projectTitle,
+                    titlePos.getX() + 5,
+                    titlePos.getY() + projectTitle.getHeight()
+            );
+        }
+    }
+
+    @FXML
+    private void onConfigure() {
+
+    }
+
+    @FXML
+    private void onSave() {
+
+    }
+
+    private void close() {
+        Stage stage = TtEdit.getPrimaryStage();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/io/github/redstonemango/ttedit/fxml/project-list.fxml"));
+        Scene scene;
+        try {
+            scene = new Scene(fxmlLoader.load());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Platform.runLater(() -> {
+            UXUtilities.applyStylesheet(scene);
+            stage.setTitle("ttEdit");
+            stage.setMaximized(false);
+            stage.setScene(scene);
+
+            UXUtilities.defineMinSize(stage);
+        });
+    }
 }
