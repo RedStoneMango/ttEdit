1a6613ec49fbf5046f32b8136bf73079984721c4
RedstoneMango <fabian@familie-krohn.de>
2025-12-22 14:17:57 +0100

    add basic SoundsPropertyItem and SoundsPropertyEditor

---
+++ src/main/java/io/github/redstonemango/ttedit/front/propertySheetHelpers/SoundsPropertyItem.java
@@ -0,0 +1,26 @@
+package io.github.redstonemango.ttedit.front.propertySheetHelpers;
+
+import io.github.redstonemango.ttedit.back.Project;
+import io.github.redstonemango.ttedit.back.projectElement.Sound;
+import javafx.beans.property.MapProperty;
+import javafx.beans.property.SetProperty;
+
+import java.util.Set;
+
+public class SoundsPropertyItem extends SimplePropertyItem {
+    private final Project project;
+    public SoundsPropertyItem(String name, String category, String description, SetProperty<Sound> sounds,
+                              Project project) {
+        super(name, category, description, sounds);
+        this.project = project;
+    }
+    public SoundsPropertyItem(String name, String category, String description, SetProperty<Sound> sounds,
+                              Project project, boolean disabled) {
+        super(name, category, description, sounds, disabled);
+        this.project = project;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+}
