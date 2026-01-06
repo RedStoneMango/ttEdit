254b65637c90de20544d468ec83b98e396a46017
RedstoneMango <fabian@familie-krohn.de>
2026-01-06 12:44:56 +0100

    implement song rendering in UI

--- src/main/java/io/github/redstonemango/ttedit/front/controller/ProjectSoundManageController.java
+++ src/main/java/io/github/redstonemango/ttedit/front/controller/ProjectSoundManageController.java
@@ -2,10 +2,14 @@ package io.github.redstonemango.ttedit.front.controller;
 
 import io.github.redstonemango.ttedit.back.Project;
 import io.github.redstonemango.ttedit.back.Sound;
+import io.github.redstonemango.ttedit.front.UXUtilities;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 
 public class ProjectSoundManageController {
 
@@ -15,7 +19,17 @@ public class ProjectSoundManageController {
     @FXML private Button deleteButton;
 
     public void init(Project project) {
-
+        header.setText("Mange Sounds for '" + project.name() + "'");
+        soundsView.setItems(project.getSounds());
+        UXUtilities.applyCustomCellFactory(soundsView, sound -> {
+            Label name = new Label(sound.name());
+            name.setFont(new Font(19));
+            Label file = new Label(sound.soundFile().getName());
+            file.setFont(new Font(14));
+            VBox box = new VBox(name, file);
+            box.setPadding(new Insets(5));
+            return box;
+        }, _ -> {}, new Insets(0));
     }
 
     @FXML
