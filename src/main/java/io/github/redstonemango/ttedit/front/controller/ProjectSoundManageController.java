fcbfe9dd745e398712ab76ed6146984b43310c16
RedstoneMango <fabian@familie-krohn.de>
2026-01-06 13:28:34 +0100

    implement song adding, deletion and renaming functionalities

--- src/main/java/io/github/redstonemango/ttedit/front/controller/ProjectSoundManageController.java
+++ src/main/java/io/github/redstonemango/ttedit/front/controller/ProjectSoundManageController.java
@@ -1,15 +1,23 @@
 package io.github.redstonemango.ttedit.front.controller;
 
+import io.github.redstonemango.ttedit.TtEdit;
 import io.github.redstonemango.ttedit.back.Project;
+import io.github.redstonemango.ttedit.back.ProjectIO;
 import io.github.redstonemango.ttedit.back.Sound;
 import io.github.redstonemango.ttedit.front.UXUtilities;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ProjectSoundManageController {
 
@@ -31,6 +39,13 @@ public class ProjectSoundManageController {
             box.setPadding(new Insets(5));
             return box;
         }, _ -> {}, new Insets(0));
+
+        playButton.disableProperty().bind(renameButton.disableProperty());
+        renameButton.disableProperty().bind(deleteButton.disableProperty());
+        deleteButton.disableProperty().bind(Bindings.createBooleanBinding(
+                () -> soundsView.getSelectionModel().getSelectedItem() == null,
+                soundsView.getSelectionModel().selectedItemProperty())
+        );
     }
 
     @FXML
@@ -39,14 +54,55 @@ public class ProjectSoundManageController {
     }
     @FXML
     private void onRename() {
+        Sound sound = soundsView.getSelectionModel().getSelectedItem();
+        if (sound == null) return;
 
+        TextInputDialog dialog = new TextInputDialog(sound.name());
+        dialog.setTitle("Rename");
+        dialog.setHeaderText("Please enter a name for the sound");
+        dialog.setContentText("Name: ");
+        UXUtilities.applyStylesheet(dialog);
+        dialog.showAndWait();
+        if (dialog.getResult() != null) {
+            String name = dialog.getResult();
+            int i = 1;
+            Set<String> names = soundsView.getItems().stream()
+                    .filter(s -> s != sound)
+                    .map(s -> s.soundFile().getName())
+                    .collect(Collectors.toSet());
+            while (names.contains(name + ".mp3")) {
+                name = "_" + i;
+                i++;
+            }
+            ProjectIO.renameSound(
+                    Project.getCurrentProject(),
+                    sound,
+                    name + ".mp3",
+                    e -> UXUtilities.errorAlert("Error renaming sound", e.getMessage())
+            );
+            soundsView.refresh();
+        }
     }
     @FXML
     private void onDelete() {
+        Sound sound = soundsView.getSelectionModel().getSelectedItem();
+        if (sound == null) return;
 
+        UXUtilities.confirmationAlert(
+                "Delete Sound",
+                "Do you really want to delete '" + sound.name() + "'?",
+                () -> {
+                    ProjectIO.removeSound(
+                            Project.getCurrentProject(),
+                            sound,
+                            e -> UXUtilities.errorAlert("Error deleting sound", e.getMessage())
+                    );
+                    soundsView.refresh();
+                });
     }
     @FXML
     private void onAdd() {
-
+        UXUtilities.showAddSoundUI(Project.getCurrentProject(), playButton.getScene().getWindow());
+        soundsView.refresh();
     }
 }
