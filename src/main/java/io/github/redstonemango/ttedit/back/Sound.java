8b7a7a639a47495fbd0106702fda379b54e512c8
RedstoneMango <fabian@familie-krohn.de>
2026-01-06 13:25:27 +0100

    add sound management methods to ProjectIO and implement fallback for sound name creation in Sound.java

--- src/main/java/io/github/redstonemango/ttedit/back/Sound.java
+++ src/main/java/io/github/redstonemango/ttedit/back/Sound.java
@@ -4,6 +4,14 @@ import java.io.File;
 
 public record Sound(String name, File soundFile) {
     public Sound(File soundFile) {
-        this(soundFile.getName().substring(0, soundFile.getName().lastIndexOf(".")), soundFile);
+        this(
+                soundFile.getName().contains(".")
+                ?
+                soundFile.getName().substring(0, soundFile.getName().lastIndexOf("."))
+                :
+                soundFile.getName(),
+            soundFile
+        );
+
     }
 }
