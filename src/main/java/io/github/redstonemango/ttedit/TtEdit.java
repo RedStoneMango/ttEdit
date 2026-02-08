ff6bff871a7cdc25f3192daa8a88e2e3e1d7f083
RedStoneMango <RedStoneMango@outlook.com>
2026-02-08 16:59:52 +0100

    remove hard-check for tttool installation on app startup

--- src/main/java/io/github/redstonemango/ttedit/TtEdit.java
+++ src/main/java/io/github/redstonemango/ttedit/TtEdit.java
@@ -1,15 +1,9 @@
 package io.github.redstonemango.ttedit;
 
-import io.github.redstonemango.mangoutils.OperatingSystem;
-import io.github.redstonemango.ttedit.back.TttoolSubprocess;
 import io.github.redstonemango.ttedit.front.UXUtilities;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonBar;
-import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,45 +14,18 @@ public class TtEdit extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        if (checkTttool()) {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/io/github/redstonemango/ttedit/fxml/project-list.fxml"));
-            Scene scene = new Scene(fxmlLoader.load());
-            UXUtilities.applyStylesheet(scene);
-            stage.setTitle("ttEdit");
-            stage.setScene(scene);
-            stage.show();
-            primaryStage = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/io/github/redstonemango/ttedit/fxml/project-list.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        UXUtilities.applyStylesheet(scene);
+        stage.setTitle("ttEdit");
+        stage.setScene(scene);
+        stage.show();
+        primaryStage = stage;
 
-            UXUtilities.defineMinSize(stage);
-        }
+        UXUtilities.defineMinSize(stage);
     }
 
     public static Stage getPrimaryStage() {
         return primaryStage;
-    }
-
-    private boolean checkTttool() {
-        if (TttoolSubprocess.findTttool() == null) {
-            System.err.println("Unable to find an installation of tttool in the PATH! Application cannot start correctly.");
-
-            ButtonType infoButton = new ButtonType("Installation Guide");
-            ButtonType closeButton = new ButtonType("Close", ButtonBar.ButtonData.CANCEL_CLOSE);
-
-            Alert alert = new Alert(Alert.AlertType.ERROR, "", infoButton, closeButton);
-            alert.setTitle("No TTTool found");
-            alert.setHeaderText("Could not find an installation of TTTool in the PATH. This is required to start");
-            alert.setContentText("Please make sure you have a valid TTTool installation named 'tttool' in your system PATH");
-            UXUtilities.applyStylesheet(alert);
-            alert.showAndWait();
-            if (alert.getResult() == infoButton) {
-                OperatingSystem.loadCurrentOS().open(
-                        "https://github.com/RedStoneMango/ttEdit/blob/main/InstallingTttool.md#-" +
-                                (OperatingSystem.isWindows() ? "Windows" : (OperatingSystem.isMac() ? "macOS" : "Linux")));
-            }
-
-            Platform.exit();
-            return false;
-        }
-        return true;
     }
 }
